a1876968c0801c23bd73ab4584139fbacdff2a84
dp84 <122209357+devarshiPatel84@users.noreply.github.com>
2025-07-03 03:16:30 +0530

    initial setup and adding backend files for authentication

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+const asyncHandler = require('express-async-handler');
+const generateToken = require('../utils/generateToken');
+
+router.post('/signup', asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(400).json({ message: 'Invalid user data' });
+    }
+ 
+}));
+
+router.post('/login', asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(401).json({ message: 'Invalid email or password' });
+    }
+}));
+
+module.exports = router;
