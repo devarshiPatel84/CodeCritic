3d0e84bc213f0ffc756139b780a205f8ddc57a41
dp84 <122209357+devarshiPatel84@users.noreply.github.com>
2025-07-12 15:11:43 +0530

    workaround for the Resize observer issue in the code editor.

---
+++ frontend/craco.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    client: {
+      overlay: {
+        runtimeErrors: (error) => {
+          if (error.message === 'ResizeObserver loop completed with undelivered notifications.') {
+            return false; 
+          }
+          return true;
+        },
+      },
+    },
+  },
+};
